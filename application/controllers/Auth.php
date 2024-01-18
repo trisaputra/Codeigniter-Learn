981fb5812afecae078e277071068a9fd6ee29b28
unknown <rsaputra35@gmail.com>
2024-01-18 16:56:03 +0700

    registration on progress

--- application/controllers/Auth.php
+++ application/controllers/Auth.php
@@ -3,6 +3,12 @@ defined('BASEPATH') or exit('No direct script access allowed');
 
 class Auth extends CI_Controller
 {
+	public function __construct()
+	{
+		parent::__construct();
+		$this->load->library('form_validation');
+	}
+
 	public function index()
 	{
 		$this->load->view('templates/auth_header');
@@ -12,8 +18,16 @@ class Auth extends CI_Controller
 
 	public function registration()
 	{
-		$this->load->view('templates/auth_header');
-		$this->load->view('auth/registration');
-		$this->load->view('templates/auth_footer');
+		$this->form_validation->set_rules('name', 'Name', 'required|trim');
+		$this->form_validation->set_rules('email', 'Email', 'required|trim|valid_email|is_unique[user.email]');
+
+		if ($this->form_validation->run() == false) {
+			$data['title'] = 'Halaman Registration';
+			$this->load->view('templates/auth_header', $data);
+			$this->load->view('auth/registration');
+			$this->load->view('templates/auth_footer');
+		} else {
+			echo 'date berhasih ditambahkan!';
+		}
 	}
 }
--- application/views/auth/registration.php
+++ application/views/auth/registration.php
@@ -9,12 +9,14 @@
                         <div class="text-center">
                             <h1 class="h4 text-gray-900 mb-4">Create an Account!</h1>
                         </div>
-                        <form class="user">
+                        <form class="user" method="post" action="<?= base_url('auth/registration'); ?>">
                             <div class="form-group">
                                 <input type="text" class="form-control form-control-user" id="name" name="name" placeholder="Full Name">
+                                <?= form_error('name', '<small class="text-danger pl-3">', '</small>'); ?>
                             </div>
                             <div class="form-group">
                                 <input type="text" class="form-control form-control-user" id="email" name="email" placeholder="Email Address">
+                                <?= form_error('email', '<small class="text-danger pl-3">', '</small>'); ?>
                             </div>
                             <div class="form-group row">
                                 <div class="col-sm-6 mb-3 mb-sm-0">
